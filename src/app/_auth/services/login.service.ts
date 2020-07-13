4804788e40488b410761a3c9a95199bce639a89e
Noam Ben Shabat <n0556771785@gmail.com>
2020-07-13 12:55:02 +0300

    OAuth2 - first try

--- src/app/_auth/services/login.service.ts
+++ src/app/_auth/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -7,23 +7,35 @@ import { map } from 'rxjs/operators';
 import { Authorize } from '../models/backend/authorize';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   login(name: string, password: string): Observable<any> {
+    const formData = new FormData();
+    formData.append('username', name);
+    formData.append('password', password);
+    formData.append('grant_type', 'password');
+    formData.append('scope', 'read');
+    const headers = new HttpHeaders({
+      Authorization: `Basic ${btoa(name)}`,
+      Username: name,
+      Password: password,
+      'Content-Type': 'multipart/form-data',
+    });
     // TODO: noamb - move base url - 185.185.126.15:8089 - to environment
     // TODO: noamb - wrap httpClient in a base service - getting the url desired, returning known response model
-    return this.httpClient.post(`http://185.185.126.15:8080/engine/users/authorize`, {
-      name,
-      password
-    }).pipe(
-      map((response: Authorize) => {
-        sessionStorage.setItem('token', response.accessToken);
-        return true;
+    return this.httpClient
+      .post(`http://185.185.126.15:8080/engine/oauth/token`, formData, {
+        headers,
+        withCredentials: true,
       })
-    );
+      .pipe(
+        map((response: Authorize) => {
+          sessionStorage.setItem('token', response.accessToken);
+          return true;
+        })
+      );
   }
 }
