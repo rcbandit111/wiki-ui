a62ba76e03570736b65355ddd4222cc1013e95ae
Noam Ben Shabat <n0556771785@gmail.com>
2020-07-20 23:31:28 +0300

    success OAuth2 request

--- src/app/_auth/services/login.service.ts
+++ src/app/_auth/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -13,21 +13,21 @@ export class LoginService {
   constructor(private httpClient: HttpClient) {}
 
   login(name: string, password: string): Observable<any> {
-    const formData = new FormData();
-    formData.append('username', name);
-    formData.append('password', password);
-    formData.append('grant_type', 'password');
-    formData.append('scope', 'read');
+    const body = new HttpParams()
+    .set('username', name)
+    .set('password', password)
+    .set('grant_type', 'password')
+    .set('scope', 'read');
     const headers = new HttpHeaders({
       Authorization: `Basic ${btoa(`${name}:${password}`)}`,
       username: name,
       password,
-      // 'Content-Type': 'multipart/form-data',
+      'Content-Type': 'application/x-www-form-urlencoded',
     });
     // TODO: noamb - move base url - 185.185.126.15:8089 - to environment
     // TODO: noamb - wrap httpClient in a base service - getting the url desired, returning known response model
     return this.httpClient
-      .post(`http://185.185.126.15:8080/engine/oauth/token`, formData, {
+      .post(`http://185.185.126.15:8080/engine/oauth/token`, body.toString(), {
         headers,
         withCredentials: true,
       })
