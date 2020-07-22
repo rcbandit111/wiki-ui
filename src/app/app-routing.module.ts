7d60fdc9806378f1b8b2df4ba8f1cdedbfbb0202
Noam Ben Shabat <n0556771785@gmail.com>
2020-07-22 23:37:21 +0300

    route to new page dashboard on success login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  {path: '',pathMatch: 'full',redirectTo: 'account' },
-   {path: 'account',loadChildren : () => import('../app/_auth/components/login.module').then(m => m.LoginModule)}
-
+  { path: '', pathMatch: 'full', redirectTo: 'account' },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('../app/_auth/components/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./_dashboard/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
